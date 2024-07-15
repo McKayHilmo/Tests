0b312b507b107511bb3539d5d049bc09c0b17709
McKay <kayhilmo@gmail.com>
2024-07-15 12:19:07 -0600

    Added window

---
+++ Program.cpp
@@ -0,0 +1,32 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <iostream>
+#include "Window.cpp"
+
+int main(){
+
+    std::cout << "Creating Window...\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+
+    while (running)
+    {
+        if(!pWindow->ProcessMessages()){
+
+            std::cout << "Closing Window...\n";
+            running = false;
+        }
+
+        //Render
+
+        Sleep(10);
+    }
+    
+    delete pWindow;
+
+    return 0;
+}
---
+++ Window.h
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+class Window
+{
+private:
+    HINSTANCE  m_hInstance;
+    HWND m_hWnd;
+public:
+    Window();
+    ~Window();
+    bool ProcessMessages();
+};
+
+
+
+
